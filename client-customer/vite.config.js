189a78315c99830e3e8e866947954365f23f5fa5
nguyen010 <maivan7895@gmail.com>
2026-04-03 20:25:54 +0700

    deploy chuẩn

--- client-customer/vite.config.js
+++ client-customer/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/doancuoikyTHW/", // 👈 thêm dòng này
+
   server: {
     port: 3001,
     proxy: {
-      '/api': 'http://localhost:3000'}
+      '/api': 'http://localhost:3000'
+    }
   }
 })
